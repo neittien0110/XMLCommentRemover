2e749a1343d8b9172e6fffebfb1b2906279f1eb1
Nguyen Duc Tien <neittien0110@gmail.com>
2021-09-14 11:52:31 +0700

    The first release

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XMLCommentRemover
+{
+    class Program
+    {
+        /// <summary>
+        ///    The name of input XML file
+        /// </summary>
+        /// <remarks>
+        ///     Obtained from a commandline param
+        /// </remarks>
+        static string victimFileName;
+
+        /// <summary>
+        ///    The name of output XML file (without comments)
+        /// </summary>
+        static string outputFileName;
+
+        /// <summary>
+        ///     The main program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            /// Must have a commandline param.
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Remove all comment from XML file.");
+                Console.WriteLine("Syntax: removecomment.exe <filename> [output]");
+                return;
+            }
+            else
+            {
+                victimFileName = args[0];
+            }
+
+
+            /// Get the output file name
+             outputFileName = (args.Length >= 2)?(args[2]):victimFileName;
+
+            /// Create Setting to read xml without comment and no blankline after removing comment.
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreComments = true;
+            settings.IgnoreWhitespace = true;
+
+            /// Readxml and parse
+            XmlReader reader = XmlReader.Create(victimFileName, settings);
+
+            /// Parse xml
+            XDocument doc = XDocument.Load(reader); // Or whatever
+
+            /// Save to file
+            reader.Close();
+            doc.Save(outputFileName);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # XMLCommentRemover
+
 This simple tool used to remove all comments from XML file. It may be useful to public a sensitive XML.
+Avaiable on Windows OS and Linux OS.
+
+Case studies:
+
+- You'd like to public a manifest.xml file of a project
+
+## Syntax
+
+```dotnetcli
+    removecomment.exe <filename> [output]
+```
+
+Examples:
+
+```dotnetcli
+    removecomment.exe manifest.xml
+    removecomment.exe manifest.xml manifest.public.xml
+```
+
+## Programming
+
+- .Net Core 3.0
+
+## License
+
+ MIT License
